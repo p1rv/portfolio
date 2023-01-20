a81da4b8a9b0b4896fa826cf404b9b8a04ee9313
p1rv <karol.p.krol@gmail.com>
2023-01-20 23:09:36 +0100

    updated root background color

--- src/components/StarryBackgroundWrapper.tsx
+++ src/components/StarryBackgroundWrapper.tsx
@@ -6,7 +6,7 @@ export const StarryBackgroundWrapper: React.FC<PropsWithChildren> = ({ children 
   return (
     <div className="">
       <div
-        className="stars-wrapper fixed inset-0 z-[-1] w-[110vw] h-[110vh] -translate-y-[5vh] -translate-x-[5vw]"
+        className="stars-wrapper fixed inset-0 w-[110vw] h-[110vh] -translate-y-[5vh] -translate-x-[5vw]"
         style={{
           background: "radial-gradient(circle at 50% 150%, #0a1234, black)",
         }}
