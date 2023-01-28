1e6a7aaffc77b92a6f4389ccd5638329006eb022
p1rv <karol.p.krol@gmail.com>
2023-01-28 18:50:00 +0100

    updated main page text

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,24 +1,42 @@
 import { useContext } from "react";
-import { ILanguageObject, LanguageContext } from "../context/LanguageProvider";
+import { ILanguageObject, IMessagesWithLanguage, LanguageContext } from "../context/LanguageProvider";
 import { MouseOverContextProvider } from "./MouseOverContextProvider";
 import { ReactCode } from "./ReactCode";
 import { WeatherDemo } from "./WeatherDemo";
 
-const welcomeMessage: ILanguageObject = {
-  EN: (
-    <>
-      Demonstrative portfolio project built with React, Typescript and Vite.
-      <br />
-      Explore available functionalities in the top-right corner.
-    </>
-  ),
-  PL: (
-    <>
-      Pokazowy projekt zbudowany na bibliotece React z Typescriptem przy pomocy Vite.
-      <br />
-      Dalsze funkcjonalności dostępne w prawym górnym rogu.
-    </>
-  ),
+const welcomeMessage: IMessagesWithLanguage = {
+  sub: {
+    EN: (
+      <>
+        Demonstrative portfolio project built with React, Typescript and Vite.
+        <br />
+        Explore available functionalities in the top-right corner.
+      </>
+    ),
+    PL: (
+      <>
+        Pokazowy projekt zbudowany na bibliotece React z Typescriptem przy pomocy Vite.
+        <br />
+        Dalsze funkcjonalności dostępne w prawym górnym rogu.
+      </>
+    ),
+  },
+  main: {
+    EN: (
+      <>
+        Karol Król -
+        <br />
+        React Project
+      </>
+    ),
+    PL: (
+      <>
+        Karol Król -
+        <br />
+        Projekt React
+      </>
+    ),
+  },
 };
 
 export const LandingPage: React.FC = () => {
@@ -26,12 +44,8 @@ export const LandingPage: React.FC = () => {
   return (
     <div>
       <div className="animate-[slideIn1_1s_ease-in-out]">
-        <p className="text-7xl sm:text-4xl lg:text-5xl lg:py-12 text-center leading-[75px] py-16">
-          Karol Król - Demo
-          <br />
-          React Project
-        </p>
-        <p className="text-center text-theme-2 sm:px-[2vw]">{welcomeMessage[language]}</p>
+        <p className="text-7xl sm:text-4xl lg:text-5xl lg:py-12 text-center leading-[75px] py-16">{welcomeMessage.main[language]}</p>
+        <p className="text-center text-theme-2 sm:px-[2vw]">{welcomeMessage.sub[language]}</p>
         <div className="relative w-[80vw] sm:w-full">
           <MouseOverContextProvider>
             <ReactCode />
