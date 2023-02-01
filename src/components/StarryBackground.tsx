2064ebd180206ec4c21d89b172101035072ac6ae
p1rv <karol.p.krol@gmail.com>
2023-02-01 01:19:02 +0100

    reduced background blur

--- src/components/StarryBackground.tsx
+++ src/components/StarryBackground.tsx
@@ -4,7 +4,7 @@ const StarryBackground: React.FC = () => {
   const renderedStars = new Array(100).fill(0).map((_, i) => <SingleStar key={i} />);
   return (
     <div
-      className="stars-wrapper fixed inset-0 w-[130vw] h-[130vh] -translate-y-[15vh] -translate-x-[15vw] z-[-1] blur-sm"
+      className="stars-wrapper fixed inset-0 w-[130vw] h-[130vh] -translate-y-[15vh] -translate-x-[15vw] z-[-1] blur-[2px]"
       style={{
         background: "radial-gradient(circle at 50% 150%, #0a1234, black)",
       }}
