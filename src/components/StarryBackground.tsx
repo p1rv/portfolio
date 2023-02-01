f803195dd454b0a62170b9105200afc132f198f6
p1rv <karol.p.krol@gmail.com>
2023-02-01 01:13:09 +0100

    (test) lazy load on background component wrapper

---
+++ src/components/StarryBackground.tsx
@@ -0,0 +1,17 @@
+import { SingleStar } from "./SingleStar";
+
+const StarryBackground: React.FC = () => {
+  const renderedStars = new Array(100).fill(0).map((_, i) => <SingleStar key={i} />);
+  return (
+    <div
+      className="stars-wrapper fixed inset-0 w-[130vw] h-[130vh] -translate-y-[15vh] -translate-x-[15vw] z-[-1] blur-sm"
+      style={{
+        background: "radial-gradient(circle at 50% 150%, #0a1234, black)",
+      }}
+    >
+      {renderedStars}
+    </div>
+  );
+};
+
+export default StarryBackground;
--- src/components/StarryBackgroundWrapper.tsx
+++ src/components/StarryBackgroundWrapper.tsx
@@ -1,19 +1,21 @@
-import { PropsWithChildren } from "react";
-import { SingleStar } from "./SingleStar";
+import { lazy, PropsWithChildren, Suspense } from "react";
+const StarryBackground = lazy(() => import("./StarryBackground"));
 
 export const StarryBackgroundWrapper: React.FC<PropsWithChildren> = ({ children }) => {
-  const renderedStars = new Array(100).fill(0).map((_, i) => <SingleStar key={i} />);
-
   return (
     <div>
-      <div
-        className="stars-wrapper fixed inset-0 w-[130vw] h-[130vh] -translate-y-[15vh] -translate-x-[15vw] z-[-1] blur-sm"
-        style={{
-          background: "radial-gradient(circle at 50% 150%, #0a1234, black)",
-        }}
+      <Suspense
+        fallback={
+          <div
+            className="stars-wrapper fixed inset-0 w-[130vw] h-[130vh] -translate-y-[15vh] -translate-x-[15vw] z-[-1] blur-sm"
+            style={{
+              background: "radial-gradient(circle at 50% 150%, #0a1234, black)",
+            }}
+          />
+        }
       >
-        {renderedStars}
-      </div>
+        <StarryBackground />
+      </Suspense>
       {children}
     </div>
   );
