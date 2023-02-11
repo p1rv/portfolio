95c486f8b26b255bb6e1840647fff18d02aefaa6
p1rv <73432351+p1rv@users.noreply.github.com>
2023-02-11 11:52:58 +0100

    Removed lazy app load

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,19 @@
 import "./index.css";
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { StarryBackgroundWrapper } from "./components/StarryBackgroundWrapper";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import { LoadingFallback } from "./components/LoadingFallback";
-const App = lazy(() => import("./App"));
+import App from "./App";
 
 createRoot(document.querySelector("#root") as HTMLDivElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <StarryBackgroundWrapper>
         <HashRouter>
-          <Suspense fallback={<LoadingFallback />}>
             <App />
-          </Suspense>
         </HashRouter>
       </StarryBackgroundWrapper>
     </Provider>
